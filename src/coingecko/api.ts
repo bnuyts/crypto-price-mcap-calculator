c90b9c8d34ce1035dbe017c9e5234f78d503eb86
b <b@b.n>
2021-11-13 10:41:59 +0100

    fix: remove stable coin mcap calculation

--- src/coingecko/api.ts
+++ src/coingecko/api.ts
@@ -35,5 +35,5 @@ export const getCoinList = () =>
 
 export const getMarketData = () =>
   axios.get<MarketData[]>(
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false'
   );
--- src/mcap/calc.ts
+++ src/mcap/calc.ts
@@ -7,8 +7,25 @@ import {
 
 export const calc = express.Router();
 
-const formatLocale = (number: Number): string =>
+const isStableCoin = (symbol: string) =>
+  ['usdt', 'busd', 'usdc', 'tusd', 'frax', 'lusd'].some((s) => s === symbol);
+
+const getFractionSize = (number: number) => {
+  const fractionCapture = /(?<=\.).*/;
+  const capture = fractionCapture.exec(number.toString());
+  if (capture) {
+    return capture[0].length;
+  }
+  return undefined;
+};
+
+const formatLocale = (
+  number: number,
+  maximumFractionDigits: number = 0
+): string =>
+  '$' +
   number.toLocaleString('en', {
+    maximumFractionDigits,
     useGrouping: true,
   });
 
@@ -16,7 +33,11 @@ const calculatePossibleMcap = (
   targetMcap: number = 1,
   tokenMcap: number,
   tokenPrice: number
-): string => formatLocale((targetMcap / tokenMcap) * tokenPrice);
+): string =>
+  formatLocale(
+    (targetMcap / tokenMcap) * tokenPrice,
+    getFractionSize(tokenPrice)
+  );
 
 calc.get('/:token', async (req, res) => {
   const compareTo = (req.query.compareto as string) || 'bitcoin';
@@ -45,16 +66,18 @@ calc.get('/', async (req, res) => {
   const ethData = marketData.find((md) => md.symbol === 'eth');
   marketData.forEach((emd: ExtendedMarketData) => {
     emd.formatted_price = formatLocale(emd.current_price);
-    emd.price_in_btc_mcap = calculatePossibleMcap(
-      btcData?.market_cap,
-      emd.market_cap,
-      emd.current_price
-    );
-    emd.price_in_eth_mcap = calculatePossibleMcap(
-      ethData?.market_cap,
-      emd.market_cap,
-      emd.current_price
-    );
+    if (!isStableCoin(emd.symbol)) {
+      emd.price_in_btc_mcap = calculatePossibleMcap(
+        btcData?.market_cap,
+        emd.market_cap,
+        emd.current_price
+      );
+      emd.price_in_eth_mcap = calculatePossibleMcap(
+        ethData?.market_cap,
+        emd.market_cap,
+        emd.current_price
+      );
+    }
   });
   res.send(marketData);
 });
